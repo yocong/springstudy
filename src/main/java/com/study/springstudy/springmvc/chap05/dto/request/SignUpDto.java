c925c8f81a73345d2f043ddc9d2259e8a951f4a6
yocong <rnfaos77@naver.com>
2024-05-30 17:35:46 +0900

    # JAVA에서 검증 + 메시지 가능 Controller에서 BindingResult를 사용하면 쓸 수 있음

--- src/main/java/com/study/springstudy/springmvc/chap05/dto/request/SignUpDto.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/dto/request/SignUpDto.java
@@ -4,6 +4,7 @@ import com.study.springstudy.springmvc.chap05.entity.Member;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter @Setter @ToString
@@ -13,8 +14,10 @@ import javax.validation.constraints.Size;
 @EqualsAndHashCode
 public class SignUpDto {
 
-    @NotBlank
-    @Size(min = 4, max =14) private String account;
+    @NotBlank(message = "아이디는 필수값입니다.")
+    @Size(min = 4, max =14, message = "아이디는 4~14글자")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문과 숫자만 포함해야 합니다.")
+    private String account;
 
     @NotBlank
     private String password;
